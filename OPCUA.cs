0618a1511a1e94a4cbce76a1539d94495bea4be4
dinesh <dinesh@metamation.com>
2023-09-20 13:26:07 +0530

    Case 24220: Changed implementation based on specification change

--- OPCUA.cs
+++ OPCUA.cs
@@ -16,7 +16,7 @@ record Packet ([property: JsonPropertyName ("nodeType")] int NodeType,
    [JsonPropertyName ("nodeName")]
    public string NodeName => $"35.{State}";
    [JsonPropertyName ("nodeValue")]
-   public string Value => "true";
+   public string Value { get; set; } = "true";
 }
 
 // Used in Settings.json
@@ -37,6 +37,9 @@ class TrumpfOPCUA : IPgmState, IInitializable, IWhiteboard {
          bool saveSettings = mSettings == null;
          mSettings ??= new (23591, 1);
          if (saveSettings) File.WriteAllText (settingsPath, JsonSerializer.Serialize (mSettings));
+         System.Net.ServicePointManager.SecurityProtocol |= System.Net.SecurityProtocolType.Tls12 |
+                                                            System.Net.SecurityProtocolType.Tls11 |
+                                                            System.Net.SecurityProtocolType.Tls;
          mTimer.Start ();
          mTimer.Elapsed += OnTimerElapsed;
       }
@@ -45,18 +48,44 @@ class TrumpfOPCUA : IPgmState, IInitializable, IWhiteboard {
 
    public void ProgramCompleted (string pgmName, int quantity = -1) {
       if (mSettings == null) return;
-      State = EMCState.Ended;
-      Task.Delay ((int)(mSettings.PgmEndToStartInterval * 1000)).ContinueWith (a => ProgramStarted ("", 0));
+      DateTime time = DateTime.UtcNow;
+      SetState (time, EMCState.Ended); ClearState (time, EMCState.Running, EMCState.Aborted, EMCState.Stopped, EMCState.StoppedMalfunction, EMCState.StoppedOperator);
+      if (Job != null && (quantity < Job.QtyNeeded || quantity < 0))
+         Task.Delay ((int)(mSettings.PgmEndToStartInterval * 1000)).ContinueWith (a => RaiseRunning ());
+      else mPgmCompleted = true;
    }
 
    public void ProgramStarted (string pgmName, int bendNo, int quantity = -1) {
-      if (MachineStatus.Mode is EOperatingMode.SemiAuto or EOperatingMode.Auto)
-         State = EMCState.Running;
+      if (mSettings == null) return;
+      if (MachineStatus.Mode is EOperatingMode.SemiAuto or EOperatingMode.Auto) {
+         if (bendNo == 0) {
+            bool completed = mPgmCompleted;
+            mPgmCompleted = false;
+            if (!completed && Job != null && Job.QtyNeeded > 0) {
+               DateTime time = DateTime.UtcNow;
+               SetState (time, EMCState.Aborted);
+               ClearState (time, EMCState.Running, EMCState.Stopped, EMCState.StoppedMalfunction, EMCState.StoppedOperator, EMCState.Ended);
+               Task.Delay ((int)(mSettings.PgmEndToStartInterval * 1000)).ContinueWith (a => RaiseRunning ());
+               mPgmCompleted = false;
+               return;
+            }
+         }
+         RaiseRunning ();
+      }
    }
+   
+   void RaiseRunning () {
+      DateTime time = DateTime.UtcNow;
+      SetState (time, EMCState.Running);
+      ClearState (time, EMCState.Aborted, EMCState.Stopped, EMCState.StoppedMalfunction, EMCState.StoppedOperator, EMCState.Ended);
+   }
+   bool mPgmCompleted;
 
    public void ProgramStopped (string pgmName, int bendNo, int quantity = -1) {
-      if (MachineStatus.Mode is EOperatingMode.SemiAuto or EOperatingMode.Auto)
-         State = MachineStatus.IsInError ? EMCState.StoppedMalfunction : EMCState.StoppedOperator;
+      if (mPgmCompleted) return;
+      DateTime time = DateTime.UtcNow;
+      SetState (time, EMCState.Stopped, MachineStatus.IsInError ? EMCState.StoppedMalfunction : EMCState.StoppedOperator);
+      ClearState (time, EMCState.Running, EMCState.Aborted, EMCState.Ended);
    }
 
    public void BendChanged (string pgmName, int bendNo) {
@@ -72,16 +101,19 @@ class TrumpfOPCUA : IPgmState, IInitializable, IWhiteboard {
 
    public IMachineStatus MachineStatus { set => sMachineStatus = value; get => sMachineStatus!; }
    static IMachineStatus? sMachineStatus;
+
+   public IJob? Job { set => sJob = value; get => sJob; }
+   static IJob? sJob;
    #endregion
 
    #region Implementation -------------------------------------------
    string SettingsPath => Path.Combine (Environment.DataFolder, "opcua-settings.json");
 
-   void SendState (EMCState state) {
+   void SendState (EMCState state, bool value, DateTime time) {
       try {
          if (mSettings == null) return;
          string url = mSettings.UseHTTPS ? "https" : "http"; 
-         var request = sClient.PutAsJsonAsync ($"{url}://localhost:{mSettings.PortNumber}/api/OpcUaNode/UpdateNodeValue", new Packet (0, DateTime.UtcNow, state));
+         var request = sClient.PutAsJsonAsync ($"{url}://localhost:{mSettings.PortNumber}/api/OpcUaNode/UpdateNodeValue", new Packet (0, time, state) { Value = value ? "true" : "false" });
          _ = request.Result;
       } catch (Exception e) {
          Console.WriteLine (e.Message);
@@ -91,10 +123,6 @@ class TrumpfOPCUA : IPgmState, IInitializable, IWhiteboard {
    void OnTimerElapsed (object? o, ElapsedEventArgs e) {
       mTimer.Stop ();
       var mode = MachineStatus.Mode;
-      if (mMode is EOperatingMode.SemiAuto or EOperatingMode.Auto) {
-         if (mode is not EOperatingMode.SemiAuto and not EOperatingMode.Auto)
-            State = EMCState.Aborted;
-      }
       mMode = mode;
       mTimer.Start ();
    }
@@ -106,9 +134,11 @@ class TrumpfOPCUA : IPgmState, IInitializable, IWhiteboard {
    static readonly HttpClient sClient = new ();
    Settings? mSettings;
 
-   EMCState State {
-      set => Task.Run (() => SendState (value));
-   }
+   void SetState (DateTime time, params EMCState[] states) 
+      => Task.Run (() => { foreach (var state in states) SendState (state, true, time); });
+
+   void ClearState (DateTime time, params EMCState[] states)
+      => Task.Run (() => { foreach (var state in states) SendState (state, false, time); });
 
    readonly System.Timers.Timer mTimer = new (); // Timer to continuously log data
    #endregion
